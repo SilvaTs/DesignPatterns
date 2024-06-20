92cc84004ec12c783639dc66b6b9375445b66d6e
Daniel Silva <danielsilvatsi@outlook.com>
2024-06-19 21:05:41 -0300

    feat: add restaurantFacade

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Facade;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var restaurantFacade = new RestaurantFacade();
+
+        Console.WriteLine("----------------------PEDIDOS DE PIZZA DO CLIENTE----------------------------\n");
+        restaurantFacade.SearchPizzaNormal();
+        restaurantFacade.SearchVerganaPizza();
+
+        Console.WriteLine("\n----------------------PEDIDOS DE PÃO DO CLIENTE----------------------------\n");
+        restaurantFacade.SearchGarlicBread();
+        restaurantFacade.SearchGarlicBreadWithCheese();
+    }
+}
---
+++ Facade/RestaurantFacade.cs
@@ -0,0 +1,36 @@
+﻿using Facade.Interfaces;
+
+namespace Facade
+{
+    public class RestaurantFacade
+    {
+        private IPizza _pizzaSupplier;
+        private IBread _breadSupplier;
+
+        public RestaurantFacade()
+        {
+            _pizzaSupplier = new PizzaSupplier();
+            _breadSupplier = new BreadSupplier();
+        }
+
+        public void SearchPizzaNormal()
+        {
+            _pizzaSupplier.SearchVerganaNormal();
+        }
+
+        public void SearchVerganaPizza()
+        {
+            _pizzaSupplier.SearchVerganaPizza();
+        }
+
+        public void SearchGarlicBread()
+        {
+            _breadSupplier.SearchGarlicBread();
+        }
+
+        public void SearchGarlicBreadWithCheese()
+        {
+            _breadSupplier.SearchGarlicBreadWithCheese();
+        }
+    }
+}
